17a200945835f2c9e3a9d30d907d102cce4bd95c
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-01 17:26:32 -0700

    Add the config project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'custom-purple' : '#666CA3',
+			'custom-blue' : '#13183F',
+			'custom-gray' : '#83869A',
+			'custom-pink' : '#F74780',
+			'custom-pink-light' : '#FFA7C3',
+			'gradient-1' : '#F02AA6',
+			'gradient-2' : '#FF6F48',
+			'gradient-3' : '#4851FF',
+			'gradient-4' : '#F02AA6',
+		}
 	},
 	plugins: [],
 }
