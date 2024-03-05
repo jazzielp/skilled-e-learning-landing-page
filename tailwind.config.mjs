f6f4169c90660ef4e5cc068dbe5fd939a4d0a76c
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-04 17:45:36 -0700

    Finish section cards

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,7 +19,7 @@ export default {
 			'mov': '320px',
 			// => @media (min-width: 375px) { ... }
 
-			'table': '800px',
+			'table': '700px',
 			// => @media (min-width: 700px) { ... }
 
 			'desktop': '1280px',
